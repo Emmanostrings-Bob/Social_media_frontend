8baa828e0f79ce0af75b3979bc004c43e1201c9b
Emmanostrings-Bob <oluwafemiayomide852@gmail.com>
2023-09-05 06:52:25 +0100

    new

--- src/components/LeftSide/ProfileCard/ProfileCard.jsx
+++ src/components/LeftSide/ProfileCard/ProfileCard.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import {useSelector} from 'react-redux'
-import Cover from '../../../img/cover.jpg'
-import Profile  from '../../../img/profileImg.jpg'
 import {Link} from 'react-router-dom'
 import './ProfileCard.css'
 
